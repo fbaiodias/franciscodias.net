3344b7e66b66033448631663de0dbe0306e9869f
Francisco Baio Dias <xicombd@gmail.com>
2015-03-02 20:55:53 +0000

    Some minor fixes

--- client/js/app.js
+++ client/js/app.js
@@ -23,20 +23,24 @@ module.exports = {
     // or else the facebook oauth
     // doesnt work
     self.buildHTML()
-
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-    ga('create', 'UA-22227995-7', 'franciscodias.net');
-    ga('send', 'pageview');
   },
 
   // init globals
   buildGlobals: function () {
     // jquery global
     window.$ = window.jQuery = require('jquery')
+
+    // google analytics
+    // (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    // (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    // m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    // })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    // window.ga('create', 'UA-22227995-7', 'auto');
+
+
+    // window.ga('create', 'UA-22227995-7', 'franciscodias.net');
+    // window.ga('send', 'pageview');
   },
 
   buildModels: function () {},
